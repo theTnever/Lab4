58756d81b959a3a0c944af515b02fa932b5ce224
theTnever <tsentsho@yahoo.com>
2013-11-27 16:22:07 -0500

    fem updates

--- Lab4/src/PhysicalTests.hpp
+++ Lab4/src/PhysicalTests.hpp
@@ -12,30 +12,52 @@
 using namespace std;
 
 class PhysicalTests{
+
+    public:
+    PhysicalTests(){}
+
+};
+
+class vital_signs:public PhysicalTests{ //Class  will receive vital sign readings for the patient
     protected:
-    int pulse;
-    int temperature;
-    int respiratory_rate;
+
+	int heartbeatspersec;
+	int respitationrate;
+	int temperatureReading;
+
 
     public:
 
-    void vital_signs(int pulse, int temperature, int respiratory_rate);
-    void blood_pressure(void);
-    void Urine_test(void);
-};
+	void PulseRate(){}//Function will receive patient pulse rate and designate either high, low or normal.
+	void RespiratoryRate(){}//Function will receive patient respiratory rate and designate either high, low or normal.
+	void BodyTemp(){}//Function will receive patient body temperature and designate either high, low or normal.
 
-class vital_signs:public PhysicalTests{ //Function will receive vital sign readings for the patient and designate either high, low or normal.
-public:
     };
 
 class blood_pressure:public PhysicalTests{ //Blood pressure of patient will be entered and function will designate either high, low or normal.
-public:
+
+    protected:
+
+	int systolic;
+	int diastolic;
+
+    public:
+	void BP_values(){}//Function will receive patient BP and designate either high, low or normal.
+
    };
 
 
-class Urine_test:public PhysicalTests{ //Function will check if urine sample of patient any abnormalities
-public:
-   };
+class Urine_test:public PhysicalTests{ //Class will check if urine sample of patient any abnormalities
+    protected:
+
+	int Urine_pH;
+
+    public:
+	void Ions_andTraceMetals(){}//Function will check if patient urine sample has any ion or trace metal abnormalities
+	void Proteins_presence(){}//Function will check if patient urine sample has any proteins, Proteins should be absent in normal urine.
+	void Glucose _presence(){}//Function will check if patient urine sample has any glucose,glucose should be absent in normal urine.
+
+  };
 
 
 #endif /* PHYSICALTESTS_HPP_ */
